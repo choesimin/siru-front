87efe15844fd05c9870b22dd9784734420fa1bdf
Choe Simin <choesimin961009@gmail.com>
2022-12-06 12:04:10 +0900

    목록 infinite scroll이 가능하도록 Masonry layout의 구현방식 수정.

--- resource/js/post/list.js
+++ resource/js/post/list.js
@@ -4,23 +4,29 @@ function getPosts() {
   fetchGet(url.post + '/category/' + category)
     .then(response => response.json())
     .then(data => {
-      var board = document.getElementById('board');
-      var post, title, writer;
+      var grid = document.getElementById('grid');
+      var item, content, title, writer;
       for (var i = 0; i < data.length; i++) {
-        post = document.createElement('a');
-        post.setAttribute('class', 'post');
-        post.setAttribute('onclick', 'detail(' + data[i].id + ')');
+        item = document.createElement('div');
+        item.className = 'item';
+
+        content = document.createElement('div');
+        content.className = 'content';
+        content.setAttribute('onclick', 'detail(' + data[i].id + ')');
 
         title = document.createElement('h3');
         title.textContent = data[i].title;
-        post.appendChild(title);
+        content.append(title);
 
         writer = document.createElement('p');
         writer.textContent = data[i].writer.name;
-        post.appendChild(writer);
+        content.append(writer);
 
-        board.append(post);
+        item.append(content);
+        grid.append(item);
       }
+
+      resizeAllGridItems();
     });
 }
 
@@ -31,7 +37,7 @@ function detail(postId) {
 
 function setBoardTitle() {
   var category = localStorage.getItem('category');
-  var boardTitle = document.getElementById('board_title');
+  var boardTitle = document.getElementById('title');
 
   if (category === 'poem') {
     boardTitle.innerText = '작품';
@@ -40,7 +46,32 @@ function setBoardTitle() {
   }
 }
 
+
+function resizeGridItem(item){
+  var grid = document.getElementById('grid');
+  var rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  var rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+  var rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+
+  item.style.gridRowEnd = 'span ' + rowSpan;
+}
+
+function resizeAllGridItems(){
+  allItems = document.getElementsByClassName('item');
+  for (x = 0; x < allItems.length; x++) {
+    resizeGridItem(allItems[x]);
+  }
+}
+
+function resizeInstance(instance){
+  item = instance.elements[0];
+  resizeGridItem(item);
+}
+
+
 window.addEventListener('load', function() {
   getPosts();
   setBoardTitle();
+
+  window.addEventListener('resize', resizeAllGridItems);
 });
